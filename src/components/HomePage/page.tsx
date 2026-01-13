8e17e70d8beaf821a96182361847e5e377b5fa30
Kamilly <kamillysinerina@gmail.com>
2026-01-13 18:32:18 -0300

    Initial commit

---
+++ src/components/HomePage/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+  <div className="text-xl p-4 font-semibold"> 
+  <p>And if you go</p>
+  <p>I wanna go with you</p>
+  <p>And if you die</p>
+  <p>I wanna die with you</p>
+  <br/>
+  <p>Eu te amo</p>
+  
+  </div>
+)
+}
